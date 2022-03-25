d7aafe8e19049a29cf430d9d18a02ddccc680026
Ratthachat (Jung) <56621342+ratthachat@users.noreply.github.com>
2022-03-25 19:31:08 +0700

    NADACallback

--- ZSSGAN/utils/training_utils.py
+++ ZSSGAN/utils/training_utils.py
@@ -1,6 +1,34 @@
 import torch
 import math
 import random
+import os
+from pytorch_lightning.callbacks import Callback
+
+class NADACallback(Callback):
+    def __init__(self, output_dir, output_interval=0, save_interval=0 ):
+        self.current_epoch = 0
+        self.output_interval = output_interval
+        self.save_interval = save_interval
+        self.sample_dir = os.path.join(output_dir, "sample")
+        self.ckpt_dir = os.path.join(output_dir, "checkpoint")
+        
+        os.makedirs(self.sample_dir, exist_ok=True)
+        os.makedirs(self.ckpt_dir, exist_ok=True)
+        
+    def on_epoch_end(self, trainer, net):
+        self.current_epoch += 1
+        if self.output_interval > 0 and self.current_epoch % self.output_interval  == 0:
+            net.generate_samples_jupyter(self.sample_dir, use_fixed_z=True, prefix_str="dst", postfix_num=self.current_epoch)
+            
+        if (self.save_interval > 0) and (self.current_epoch > 0) and self.current_epoch % self.save_interval  == 0:
+            print('saving checkpoint at iteration', self.current_epoch)
+            torch.save(
+                {
+                "g_ema": net.generator_trainable.generator.state_dict(),
+                "g_optim": net.optimizers().state_dict(),
+                },
+                f"{self.ckpt_dir}/{str(self.current_epoch).zfill(6)}.pt",
+            )
 
 def make_noise(batch, latent_dim, n_noise, device):
     if n_noise == 1:
@@ -32,3 +60,4 @@ def g_path_regularize(fake_img, latents, mean_path_length, decay=0.01):
     path_penalty = (path_lengths - path_mean).pow(2).mean()
 
     return path_penalty, path_mean.detach(), path_lengths
+
